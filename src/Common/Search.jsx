692981250383d1530177a187d51ed001413a218a
faizalam <faizalamz880@gmail.com>
2025-06-28 19:24:56 +0530

    update

--- src/Common/Search.jsx
+++ src/Common/Search.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
-const Search = ({dataSearch}) => {
+const Search = ({ dataSearch }) => {
   const style = {
     width: "250px",
     height: "35px",
     borderRadius: "5px",
     fontSize: "15px",
     paddingLeft: "5px",
-    marginLeft: "10px"
+    marginLeft: "10px",
   };
   function changeEvent(e) {
     dataSearch(e.target.value);
@@ -15,7 +15,7 @@ const Search = ({dataSearch}) => {
   return (
     <>
       <input
-      className="bg-gray-200 border-none focus:outline-none "
+        className="bg-gray-200 border-none focus:outline-none "
         style={style}
         id="search"
         type="text"
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import List from "../Common/List";
 
 const Footer = () => {
   return (
-    <div id="Footer" >
+    <div id="Footer">
       <h1>this is footer</h1>
     </div>
   );
