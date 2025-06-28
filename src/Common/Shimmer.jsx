cb696a7783c51714e5316bd3bf338e012e71f527
faizalam <faizalamz880@gmail.com>
2025-06-28 16:34:13 +0530

    update

---
+++ src/Common/Shimmer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="flex flex-wrap gap-2.5" >
+      {Array(10)
+        .fill("")
+        .map((element, i) => {
+          return <div className="w-[240px] h-[250px] bg-gray-300 rounded-2xl m-3" key={i}>
+            {element}
+          </div>;
+        })}
+    </div>
+  );
+};
+
+export default Shimmer;
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,17 @@
 import Search from "../Common/Search";
 import Card from "../Common/Card";
+import Shimmer from "../Common/Shimmer";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-
 
 const Body = () => {
   const [data, setData] = useState([]);
   const [filteredData, setFilterData] = useState([]);
+
   useEffect(() => {
     callData();
   }, []);
+  
   async function callData() {
     const fetchData = await fetch("http://localhost:3000/getAllData");
     const res = await fetchData.json();
@@ -23,15 +25,25 @@ const Body = () => {
     });
     setFilterData(searched);
   };
-
+  if (!data[0]) {
+    return (
+      <>
+        <Shimmer />
+      </>
+    );
+  }
   return (
     <div id="Body">
       <Search dataSearch={SearchFunc} />
       <div className="cards-container">
         {filteredData.map((element, index) => {
           return (
-            <Link key={element?.id} state={element}  to={`/${element?.name}/${element?.id}`} >
-              <Card  data={element} />
+            <Link
+              key={element?.id}
+              state={element}
+              to={`/${element?.name}/${element?.id}`}
+            >
+              <Card data={element} />
             </Link>
           );
         })}
