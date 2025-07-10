22eb9f9a8255873a36979a6093dc762a95101126
faizalam <faizalamz880@gmail.com>
2025-07-10 17:20:02 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,12 +13,12 @@ const Navbar = () => {
     <div id="Navbar" className="h-20 fixed top-0 left-0 w-full z-50">
       <Link to="/">
         {/* <div className="flex items-center"> */}
-          <img
-            className="h-13 w-43"
-            src="http://localhost:3000/images/Logos.svg"
-            alt="img"
-          />
-          {/* <div className="font-extrabold text-3xl text-orange-500 ">
+        <img
+          className="h-13 w-43"
+          src="http://localhost:3000/images/Logos.svg"
+          alt="img"
+        />
+        {/* <div className="font-extrabold text-3xl text-orange-500 ">
             <h1>Snuggy</h1>
           </div> */}
         {/* </div> */}
